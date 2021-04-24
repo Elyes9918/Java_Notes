9e5e3722066fc7930574f5a483108205e9f183e5
Elyes9918 <elyes9918@gmail.com>
2021-04-24 18:38:15 +0000

    Updated

--- MoreNotes.java
+++ MoreNotes.java
@@ -234,3 +234,35 @@ public class Main {
 	}
 }
 //Java inner Classes******************************************************************************************************************************
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/* inner class = 	A class inside of another class.
+							useful if a class should be limited in scope.
+							usually private, but not necessary
+							helps group classes that belong together
+							extremely useful for listeners for specific events
+							precursor for anonymous inner classes			
+		*/
+		Outside out = new Outside();
+		Outside.Inside in = out.new Inside();
+		in.Greeting();
+	}
+//********************************************************
+public class Outside {
+
+	String x = "Hello ";
+	
+	public class Inside{
+		
+		String y = "World!";
+		
+		public void Greeting()
+		{
+			System.out.println(x+y);
+		}
+	}
+}
+
+//Java Anonymous Inner Class **********************************************************************************************************************
